4416c6bdc982681a5b79cea1c0e15d979889757b
theRamosDeveloper <104318910+theRamosDeveloper@users.noreply.github.com>
2022-11-13 21:36:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,48 +7,53 @@ const countries = {
     "pt-PT": "Português"
 }
 
-const fromText = document.querySelector(".from-text")
-toText = document.querySelector(".to-text")
-selectTag = document.querySelectorAll("select")
-exchangeIcon = document.querySelector(".exchange")
-translateBtn = document.querySelector("button")
-icons = document.querySelectorAll(".row i")
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
 
 selectTag.forEach((tag, id) => {
-    for(const country_code in countries){
-        let selected;
-        if(id == 0 && country_code == "pt-PT"){
-            selected = "selected"
-        } else if(id == 1 && country_code == "en-GB"){
-            selected = "selected"
-        }
-        let option = `<option value="${country_code}" ${selected}>${countries[country_code]}</option>`
-        tag.insertAdjacentHTML("beforeend",option)
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "pt-PT" ? "selected" : "" : country_code == "en-GB" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
     }
-})
+});
 
-exchangeIcon.addEventListener("click", ()=>{
-    let tempText = fromText.value
-    tempLang = selectTag[0].value
-    fromText.value = toText.value
-    selectTag[0].value = selectTag[1].value
-    toText.value = tempText
-    selectTag[1].value = tempLang
-})
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
 
-translateBtn.addEventListener("click", ()=> {
-    let text = fromText.value 
-    tempLang = selectTag[0].value
-    translateFrom = selectTag[0].value
-    translateTo = selectTag[1].value
-    if(!text) return
-    toText.setAttribute("placeholder", "Traduzindo...")
-    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`
+fromText.addEventListener("keyup", () => {
+    if(!fromText.value) {
+        toText.value = "";
+    }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Traduzindo...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
         toText.value = data.responseData.translatedText;
-        toText.setAttribute("placeholder", "Tradução")
-    })
-})
+        data.matches.forEach(data => {
+            if(data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Translation");
+    });
+});
 
 icons.forEach(icon => {
     icon.addEventListener("click", ({target}) => {
